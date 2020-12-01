f95eb7fd46cbf3128304c8f35bb94766f392da98
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-01 02:25:26 -0500

    Reversed intergers

---
+++ reverseint/index.js
@@ -0,0 +1,54 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    let reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n)
+
+}
+
+
+
+module.exports = reverseInt;
+
+////////////////////////////////////////////
+// SOLUTION 1
+////////////////////////////////////////////
+// function reverseInt(n) {
+//     let reversed = n.toString().split('').reverse().join('');
+//     return parseInt(reversed) * Math.sign(n)
+
+// }
+
+////////////////////////////////////////////
+// SOLUTION 2
+////////////////////////////////////////////
+// function reverseInt(n) {
+//     return parseFloat(
+//         n
+//             .toString()
+//             .split('')
+//             .reverse()
+//             .join('')) * Math.sign(n)
+
+// }
+
+////////////////////////////////////////////
+// SOLUTION 3
+////////////////////////////////////////////
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('');
+//     if (n < 0) {
+//         return parseInt(reversed) * -1
+//     }
+//     return parseInt(reversed) * 1
+
+
+// }
