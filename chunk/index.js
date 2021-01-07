c674ad80a594ebfd4c696a791dc2e9e757ad6059
ayninur <nuralaynabdulqadir@gmail.com>
2021-01-06 23:51:09 -0500

    max chars

---
+++ chunk/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    let charObj = {}
+    let max = 0;
+    let maxChar = ''
+    //loop through string and put in object with char repetition count
+    for (let char of str) {
+        charObj[char] ? charObj[char]++ : charObj[char] = 1 //in the character we're iterating over is in the object then add one if it is not then make it 1
+    }
+    for (let char in charObj) {
+        if (charObj[char] > max) {
+            max = charObj[char]
+            maxChar = char
+        }
+        return maxChar
+    }
+
+}
+
+
+module.exports = maxChar;
