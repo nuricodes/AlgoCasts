22fb699c1435a478768111518aa846d808ee64b8
ayninur <nuralaynabdulqadir@gmail.com>
2021-01-12 04:39:35 -0500

    Capitalize Solution 3

--- capitalize/index.js
+++ capitalize/index.js
@@ -21,3 +21,16 @@ module.exports = capitalize;
 // function capitalize(str) {
 //     return str.split(" ").map(n => n.charAt().toUpperCase() + n.slice(1)).join(" ")
 // }
+
+// Solution 3
+// function capitalize(str) { 
+//     let result = str[0].toUpperCase();
+//    for (let i = 1; i < str.length; i++) {
+//        if (str[i-1] === ' ') {
+//         result += str[i].toUpperCase();
+//     } else {
+//     result += str[i]
+//     }
+//    }
+//     return result
+//   }
