0e29a5f92c44b15a8fa766409aa284d01a86f587
ayninur <nuralaynabdulqadir@gmail.com>
2021-02-01 03:18:10 -0500

    recursive solution

--- steps/index.js
+++ steps/index.js
@@ -17,20 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for (let row = 0; row < n; row++) {
-        let stair = "";
-        for (let column = 0; column < n; column++) {
-            if (column <= row) {
-                stair += "#"
-            } else {
-                stair += " "
-            }
+function printNumber(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
 
-        }
-        console.log(stair)
-
+    if (stair.length < row) {
+        stair += '#';
+    } else {
+        stair += ' ';
     }
 }
 
 module.exports = steps;
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = "";
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += "#"
+//             } else {
+//                 stair += " "
+//             }
+
+//         }
+//         console.log(stair)
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
